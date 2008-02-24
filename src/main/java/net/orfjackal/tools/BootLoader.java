f60ac5b2f19f60c012578fda9e1286fbf97972e5
Esko Luontola <esko.luontola@gmail.com>
2008-02-24 20:21:06 +0000

    Externalized bootloader config

--- src/main/java/net/orfjackal/tools/BootLoader.java
+++ src/main/java/net/orfjackal/tools/BootLoader.java
@@ -2,10 +2,12 @@ package net.orfjackal.tools;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Properties;
 
 /**
  * @author Esko Luontola
@@ -13,17 +15,38 @@ import java.net.URLClassLoader;
  */
 public class BootLoader {
 
-    public static final String LIBRARY_DIR = "lib";
-    public static final String MAIN_CLASS = "com.foo.Main";
+    public static final String PROPERTIES_PATH = "/bootloader.properties";
+    public static final String LIBRARY_DIR_KEY = "libraryDir";
+    public static final String MAIN_CLASS_KEY = "mainClass";
+
+    private static String libraryDir;
+    private static String mainClass;
 
     public static void main(String[] args) throws Exception {
+        readProperties();
         URL[] libs = urlsFrom(listJarsIn(libraryDir()));
         ClassLoader classLoader = new URLClassLoader(libs, Thread.currentThread().getContextClassLoader());
-        start(MAIN_CLASS, args, classLoader);
+        start(mainClass, args, classLoader);
+    }
+
+    private static void readProperties() throws IOException {
+        Properties p = new Properties();
+        p.load(BootLoader.class.getResourceAsStream(PROPERTIES_PATH));
+        libraryDir = getRequiredProperty(p, LIBRARY_DIR_KEY);
+        mainClass = getRequiredProperty(p, MAIN_CLASS_KEY);
+    }
+
+    private static String getRequiredProperty(Properties p, String key) {
+        String value = p.getProperty(key);
+        if (value == null) {
+            throw new IllegalArgumentException(
+                    "Required key \"" + key + "\" not found from file \"" + PROPERTIES_PATH + "\"");
+        }
+        return value;
     }
 
     private static File libraryDir() {
-        File dir = new File(LIBRARY_DIR);
+        File dir = new File(libraryDir);
         if (!dir.isDirectory()) {
             throw new IllegalStateException("No such directory: " + dir);
         }
