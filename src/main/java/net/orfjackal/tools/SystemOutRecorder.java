8d5430ed119d1a64a78b030c0bf374504da9b150
Esko Luontola <esko.luontola@gmail.com>
2008-04-05 12:41:36 +0000

    SystemOutRecorder

---
+++ src/main/java/net/orfjackal/tools/SystemOutRecorder.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2008, Esko Luontola. All Rights Reserved.
+ */
+
+package net.orfjackal.tools;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * @author Esko Luontola
+ * @since 5.4.2008
+ */
+public class SystemOutRecorder {
+
+    private PrintStream realSystemOut;
+    private ByteArrayOutputStream actualBytes;
+    private ByteArrayOutputStream expectedBytes;
+    private PrintStream expected;
+
+    public void install() {
+        realSystemOut = System.out;
+        actualBytes = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actualBytes));
+        expectedBytes = new ByteArrayOutputStream();
+        expected = new PrintStream(expectedBytes);
+    }
+
+    public void uninstall() {
+        System.setOut(realSystemOut);
+        realSystemOut = null;
+        actualBytes = null;
+        expectedBytes = null;
+        expected = null;
+    }
+
+    public PrintStream expect() {
+        return expected;
+    }
+
+    public String actualOutput() {
+        return actualBytes.toString();
+    }
+
+    public String expectedOutput() {
+        return expectedBytes.toString();
+    }
+}
