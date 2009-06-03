fa3b3bd07c261b4eae4f6b71b78e39d0d847eb8d
Esko Luontola <esko.luontola@gmail.com>
2009-06-03 17:55:57 +0300

    RecursiveSpecification: refactored

--- src/main/scala/experiment/TestSpy.java
+++ src/main/scala/experiment/TestSpy.java
@@ -1,16 +1,5 @@
-/*
- * Copyright (c) 2008-2009  Esko Luontola, www.orfjackal.net
- *
- * You may use and modify this source code freely for personal non-commercial use.
- * This source code may NOT be used as course material without prior written agreement.
- */
-
 package experiment;
 
-/**
- * @author Esko Luontola
- * @since 3.6.2009
- */
 public class TestSpy {
 
     private static final ThreadLocal<StringBuffer> context = new ThreadLocal<StringBuffer>();
