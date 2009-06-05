76f9e13d45fa42d7a3e2cc0c152b1312516b06b2
Esko Luontola <esko.luontola@gmail.com>
2009-06-06 00:45:13 +0300

    Improved debug helpers

--- src/test/java/net/orfjackal/experimental/specs/TestSpy.java
+++ src/test/java/net/orfjackal/experimental/specs/TestSpy.java
@@ -2,14 +2,28 @@ package net.orfjackal.experimental.specs;
 
 public class TestSpy {
 
-    private static final ThreadLocal<StringBuffer> context = new ThreadLocal<StringBuffer>();
+    private static final ThreadLocal<StringBuffer> context = new ThreadLocal<StringBuffer>() {
+        protected StringBuffer initialValue() {
+            return new StringBuffer();
+        }
+    };
 
     public static void reset() {
-        context.set(new StringBuffer());
+        context.remove();
     }
 
     public static void append(String s) {
+//        debug(s);
         context.get().append(s);
+    }
+
+    private static void debug(String s) {
+        System.out.println("append: " + s);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static String get() {
