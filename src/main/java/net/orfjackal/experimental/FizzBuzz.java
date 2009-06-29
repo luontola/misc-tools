c318fe342096434a0581c4c03a99a68c3a1151d3
Esko Luontola <esko.luontola@gmail.com>
2009-06-30 00:47:10 +0300

    Refactored FizzBuzz

--- src/main/java/net/orfjackal/experimental/FizzBuzz.java
+++ src/main/java/net/orfjackal/experimental/FizzBuzz.java
@@ -3,14 +3,17 @@ package net.orfjackal.experimental;
 
 public class FizzBuzz {
 
+    private static final int FIZZ = 3;
+    private static final int BUZZ = 5;
+
     public static String textForNumber(int n) {
-        if (n % 15 == 0) {
+        if (n % (FIZZ * BUZZ) == 0) {
             return "FizzBuzz";
         }
-        if (n % 3 == 0) {
+        if (n % FIZZ == 0) {
             return "Fizz";
         }
-        if (n % 5 == 0) {
+        if (n % BUZZ == 0) {
             return "Buzz";
         }
         return Integer.toString(n);
