4bac156f7d69d839ec549d70c45529c866b8692b
Esko Luontola <esko.luontola@gmail.com>
2010-07-26 01:01:59 +0300

    Removed the proof-of-concept for recursive specifications

--- src/main/scala/net/orfjackal/experimental/specs/SpecRunListener.scala
+++
@@ -1,7 +0,0 @@
-package net.orfjackal.experimental.specs
-
-trait SpecRunListener {
-  def fireBeginExample(currentPath: List[Int])
-
-  def fireFinishExample(currentPath: List[Int])
-}
--- src/main/scala/net/orfjackal/experimental/specs/SpecRunResult.scala
+++
@@ -1,4 +0,0 @@
-package net.orfjackal.experimental.specs
-
-class SpecRunResult(val executedPaths: List[List[Int]], val newUnexecutedPaths: List[List[Int]]) {
-}
