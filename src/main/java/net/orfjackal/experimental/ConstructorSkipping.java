db6c64d9dbe62b5afbf09e3516918ff5cbd5bc42
Esko Luontola <esko.luontola@gmail.com>
2014-04-08 08:38:12 +0300

    Refactored

--- src/main/java/net/orfjackal/experimental/ConstructorSkipping.java
+++ src/main/java/net/orfjackal/experimental/ConstructorSkipping.java
@@ -2,6 +2,10 @@ package net.orfjackal.experimental;
 
 import org.objectweb.asm.*;
 
+/**
+ * This works on e.g. 32-bit jdk1.6.0_10 (Windows) but not on the 64-bit version or the latest JDK versions.
+ * Apparently it was a HotSpot bug.
+ */
 public class ConstructorSkipping implements Opcodes {
 
     private static final String CHILD2_CLASS = "$GENERATED$Child2";
@@ -13,12 +17,15 @@ public class ConstructorSkipping implements Opcodes {
                 defineClass(CHILD2_CLASS, child2, 0, child2.length);
             }
         };
+
         Object child1 = new Child1();
+        System.out.println("child1.getClass() = " + child1.getClass());
+        System.out.println("child1 instanceof Parent = " + (child1 instanceof Parent));
+        System.out.println("---");
+
         Object child2 = classLoader.loadClass(CHILD2_CLASS).newInstance();
-        Object parent = new Parent();
-        System.out.println("child1 = " + child1);
-        System.out.println("child2 = " + child2);
-        System.out.println("parent = " + parent);
+        System.out.println("child2.getClass() = " + child2.getClass());
+        System.out.println("child2 instanceof Parent = " + (child2 instanceof Parent));
     }
 
     public static byte[] createChild2() {
@@ -40,9 +47,8 @@ public class ConstructorSkipping implements Opcodes {
 
     public static class Parent {
         public Parent() {
+            // code with unwanted side-effects
             System.out.println("Parent's constructor was called");
-            System.out.println("getClass() = " + getClass());
-            System.out.println("--");
         }
     }
 
